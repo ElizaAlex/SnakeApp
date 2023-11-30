569ddfee8fcc48bd8ac11a1b44a76c4094024328
Mason <mschult1@terpmail.umd.edu>
2023-11-30 11:38:03 -0500

    Implemented SnakeGrid, added backend functionality to CanvasView.kt, though no display is currently happening. Backend functionality is currently untested, so testing needs to be done.

--- app/src/main/java/com/example/groupproject/CanvasView.kt
+++ app/src/main/java/com/example/groupproject/CanvasView.kt
@@ -6,11 +6,25 @@ import android.graphics.Color
 import android.graphics.Paint
 import android.view.View
 
+public val ROWS = 10
+public val COLS = 10
 class CanvasView(context: Context,rows: Int, cols: Int): View(context) {
     var paint = Paint()
+    var grid: SnakeGrid = SnakeGrid(ROWS,COLS)
+
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
         paint.color = Color.BLACK
         canvas.drawRect(0f,0f, super.getWidth().toFloat(),super.getHeight().toFloat(),paint)
+        grid.update()
+        var tail: Snake.Segment = grid.getSnake()
+        var (appleX,appleY) = grid.getApple()
+
+        //TODO - Draw the apple
+        while(tail.prev != null){
+            //TODO - Draw the snake
+        }
+
+
     }
 }
--- app/src/main/java/com/example/groupproject/snakeGrid.kt
+++ app/src/main/java/com/example/groupproject/snakeGrid.kt
@@ -1,4 +1,45 @@
 package com.example.groupproject
 
-class snakeGrid {
+import kotlin.random.Random
+
+class SnakeGrid(rows: Int, cols: Int) {
+    var rows: Int = rows
+    var cols: Int = cols
+    var snake: Snake = Snake(rows,cols)
+    var apple: Apple = makeApple()
+    var lost: Boolean = false
+
+    fun getApple(): Pair<Int,Int>{
+        return Pair(apple.x,apple.y)
+    }
+    fun getSnake(): Snake.Segment {
+        return snake.tail
+    }
+    fun update():Unit{
+        if (lost) return
+
+        var (x,y) = snake.nextLoc()
+        if(x==apple.x && y==apple.y){
+            snake.feed()
+            apple = makeApple()
+        }
+
+        var selfCollide = snake.update()
+        lost = selfCollide || snake.head.isOutOfBounds()
+    }
+    private fun makeApple(): Apple{
+        var ap : Apple = Apple(rows,cols)
+        while(snake.contains(Pair(ap.x,ap.y))){
+            ap = Apple(rows,cols)
+        }
+        return ap
+    }
+    inner class Apple{
+        var x: Int
+        var y: Int
+        constructor(rows: Int, cols: Int){
+            x=Random.nextInt(0,cols)
+            y=Random.nextInt(0,rows)
+        }
+    }
 }
