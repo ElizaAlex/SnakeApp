fa0c0f3db786650dda5aaa7fec28304e421bd6b3
Avik Rao <avik.rao@gmail.com>
2023-12-05 20:17:34 -0500

    Add banner ad to score screen

--- app/src/main/java/com/example/groupproject/EndActivity.kt
+++ app/src/main/java/com/example/groupproject/EndActivity.kt
@@ -3,7 +3,12 @@ package com.example.groupproject
 import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.LinearLayout
 import android.widget.TextView
+import com.google.android.gms.ads.AdRequest
+import com.google.android.gms.ads.AdSize
+import com.google.android.gms.ads.AdView
+import com.google.android.gms.ads.interstitial.InterstitialAd
 
 class EndActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,5 +19,17 @@ class EndActivity : AppCompatActivity() {
         val score = pref.getInt("score", 0)
 
         findViewById<TextView>(R.id.end_score_text).text = "Score: $score"
+
+        val adView = AdView(this)
+        val adSize = AdSize(AdSize.FULL_WIDTH, AdSize.AUTO_HEIGHT)
+        adView.setAdSize(adSize)
+        adView.adUnitId = "ca-app-pub-3940256099942544/6300978111"
+
+        val adRequest = AdRequest.Builder().build()
+
+        val adLayout = findViewById<LinearLayout>(R.id.ad_view)
+        adLayout.addView(adView)
+
+        adView.loadAd(adRequest)
     }
 }
