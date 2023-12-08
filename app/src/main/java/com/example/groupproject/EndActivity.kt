f75679fa95ea78f450b110982564aa194e203b24
Avik Rao <avik.rao@gmail.com>
2023-12-07 21:16:15 -0500

    Add email and replay functions

--- app/src/main/java/com/example/groupproject/EndActivity.kt
+++ app/src/main/java/com/example/groupproject/EndActivity.kt
@@ -1,8 +1,10 @@
 package com.example.groupproject
 
 import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.LinearLayout
 import android.widget.TextView
 import com.google.android.gms.ads.AdRequest
@@ -11,12 +13,15 @@ import com.google.android.gms.ads.AdView
 import com.google.android.gms.ads.interstitial.InterstitialAd
 
 class EndActivity : AppCompatActivity() {
+
+    private var score: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_end)
 
         val pref = getSharedPreferences("SnakeGame", Context.MODE_PRIVATE)
-        val score = pref.getInt("score", 0)
+        this.score = pref.getInt("score", 0)
 
         findViewById<TextView>(R.id.end_score_text).text = "Score: $score"
 
@@ -31,5 +36,20 @@ class EndActivity : AppCompatActivity() {
         adLayout.addView(adView)
 
         adView.loadAd(adRequest)
+    }
+
+    fun replay(v: View) {
+        val intent = Intent(this, SnakeActivity::class.java)
+        this.finish()
+        this.startActivity(intent)
+    }
+
+    fun email(v: View) {
+        val emailIntent = Intent(Intent.ACTION_SEND)
+            .setType("text/plain")
+            .putExtra(Intent.EXTRA_SUBJECT, "I played Snake!")
+            .putExtra(Intent.EXTRA_TEXT, "I got a score of $score!")
+            .setType("message/rfc822")
+        this.startActivity(Intent.createChooser(emailIntent, "Send score email"))
     }
 }
